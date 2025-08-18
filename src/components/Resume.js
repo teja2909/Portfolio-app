48093220ae5db769278f226e8f17b1f3f7dce908
teja2909 <tejavorsu1988@gmail.com>
2025-08-18 19:23:51 +0530

    Made changes in resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -8,10 +8,10 @@ const Resume = () => {
         <h2 class="title">My <span>Resume</span></h2>
         <div class="resume-container">
             {/* <Embed Resume Locally> */}
-            <embed src="Vorsu_Teja_Vigneshwar_Resume.pdf" title="Vorsu Teja Vigneshwar Resume" width="100%" height="500" />
+            <embed src="Vorsu_Teja_Vigneshwar.pdf" title="Vorsu Teja Vigneshwar Resume" width="100%" height="500" />
         </div>
         <div className="download-btn">
-          <a href="Vorsu_Teja_Vigneshwar_Resume.pdf" download>
+          <a href="Vorsu_Teja_Vigneshwar.pdf" download>
             Download Resume
           </a>
         </div>
